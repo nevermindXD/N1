efc18724bfa3a0123af1dd94f9447f14b7094692
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2017-07-27 10:04:01 -0500

    Version with API

---
+++ demo/API/config.php
@@ -0,0 +1,21 @@
+<?php
+// define ('DB_USR', 'tecorizaba');
+// define ('DB_PASS', 'orizaba123.');
+// define ('DB_DATABASE', 'db_projects');
+// define ('DB_HOST', '23.229.178.193');
+// define ('MAIN_URL', 'http://lsys.mx/projectsv2/');
+
+//*
+define ('DB_USR', 'lionsys');
+define ('DB_PASS', 'Syslion894');
+define ('DB_DATABASE', 'db_aguirre');
+define ('DB_HOST', '23.229.178.193');
+define ('MAIN_URL', 'http://aguirreyhernandez/aguirreyhernandez/api/');
+/* /
+define ('DB_USR', 'root');
+define ('DB_PASS', '');
+define ('DB_DATABASE', 'db_aguirre');
+define ('DB_HOST', 'localhost');
+define ('MAIN_URL', 'http://aguirreyhernandez/aguirreyhernandez/api/');
+*/
+?>
